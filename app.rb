ba9d524de4f50272f03125583b480518118cd2ca
Nicole Dow <nikki.dow71@gmail.com>
2019-08-26 15:14:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class App < Sinatra::Base
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path.match(/)
+      resp.status = 200
+    end
   end
 
 end
