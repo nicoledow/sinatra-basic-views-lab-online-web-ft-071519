f12cfe136d4beeb1b06adfc8f6e37e2cf627b735
Nicole Dow <nikki.dow71@gmail.com>
2019-08-26 15:14:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,6 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-    
-    if req.path.match(/)
-      resp.status = 200
-    end
-  end
+
 
 end
